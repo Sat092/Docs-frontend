b5857693bf1d05b4a3534fc1d04755555c90216a
Sat092 <satyamkus092@gmail.com>
2025-02-06 09:20:57 +0530

    New Feature added

--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -5,9 +5,6 @@ const Foreground = () => {
     const ref = useRef(null);
     const [cards, setCards] = useState([]);
     const [task, setTask] = useState("");
-    
-    const [num, setnum] = useState("gree")
-    const [open, setopen] = useState("t")
 
     useEffect(() => {
         const savedCards = JSON.parse(localStorage.getItem("cards"));
@@ -15,30 +12,32 @@ const Foreground = () => {
             setCards(savedCards);
         }
     }, []);
+
     useEffect(() => {
         localStorage.setItem("cards", JSON.stringify(cards));
     }, [cards]);
 
-
+    const [color, setColor] = useState('')
     var rn=Math.floor(Math.random()*2)
+
     const addCard = () => {
         if(rn===0){
-            setnum("blue")
-            setopen(true)
+            setColor('blue')
         }
         else{
-            setnum("green")
-            setopen(false)
+            setColor('green')
         }
+
+
         if (task.trim() === "") return;
         const newCard = {
             desc: task,
             filesize: ".9mb",
-            close:open,
+            close: false,
             tag: {
                 isOpen: true,
                 tagTitle: "Download Now..",
-                tagColor: num
+                tagColor: color
             },
         };
         setCards([...cards, newCard]);
@@ -47,9 +46,9 @@ const Foreground = () => {
 
     return (
         <>
-            <div ref={ref} className=' main w-full h-full fixed z-[3] top-0 left-0 flex gap-5 flex-wrap p-5 '>
+            <div ref={ref} className='main w-full h-full fixed z-[3] top-0 left-0 flex gap-5 flex-wrap p-5'>
                 {cards.map((items, index) => (
-                    <Card key={index} data={items} reference={ref}  />
+                    <Card key={index} data={items} reference={ref} />
                 ))}
                 <div className='flex flex-col items-center'>
                     <input
@@ -57,7 +56,7 @@ const Foreground = () => {
                         value={task}
                         onChange={(e) => setTask(e.target.value)}
                         placeholder="Enter your task"
-                        className='mb-5 p-2 border border-gray-300 rounded text-black'
+                        className='mb-5 p-2 border border-gray-300 rounded text-black '
                     />
                     <button onClick={addCard} className='w-10 h-10 bg-blue-500 text-white rounded-full flex items-center justify-center'>
                         +
